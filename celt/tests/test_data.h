ad831e46f5bace8900c800b4f5e2b1eb609aa695
Daparrag <daparrag@correo.udistrital.edu.co>
2018-01-23 10:32:59 +0100

    Including test data to test the kissfft on armv7e CPU

---
+++ celt/tests/test_data.h
@@ -0,0 +1,15 @@
+#ifndef __TEST_DATA_H
+#define __TEST_DATA_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+
+extern const opus_int32 real_vector[48000];
+extern const opus_int32 img_vector[48000];
+
+#endif __TEST_DATA_H
